28308162fb24bbb4b0084c401f0fa3cff3dc2e33
k2u4sh <122009631+k2u4sh@users.noreply.github.com>
2023-05-24 13:17:27 +0530

    changes
    ss

--- gunicorn-cfg.py
+++ gunicorn-cfg.py
@@ -1,11 +1,7 @@
-# -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
+import os
 
-bind = '0.0.0.0:5005'
-workers = 1
-accesslog = '-'
-loglevel = 'debug'
-capture_output = True
-enable_stdio_inheritance = True
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
+application = get_wsgi_application()
