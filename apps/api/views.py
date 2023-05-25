abb19d2a2c2006d097b08490c585374324667444
k2u4sh <122009631+k2u4sh@users.noreply.github.com>
2023-05-25 09:56:54 +0530

    changes in data

--- apps/api/views.py
+++ apps/api/views.py
@@ -17,7 +17,6 @@ pwd="po98#2hy$678G"
 engine = create_engine("mysql+pymysql://{user}:{pw}@{host}/{db}"
 				.format(host=hostname, db=dbname, user=uname, pw=pwd))
 
-
 mydb = mysql.connector.connect(
   host=hostname,
   user=uname,
@@ -36,8 +35,12 @@ def getApiValue(request):
         lastDate =  request.GET.get("lastDate")
         sql = "select * from final_data WHERE date BETWEEN '"+startDate+"' AND '"+lastDate+"'"
         graph = pd.read_sql(sql,con=engine)
+
+        mycursor.execute(sql)
+        myresult = mycursor.fetchall()
         datasets = []
         data=[]
+        graphD = []
         labels = graph.date
         data_solar = graph.solar
         data_wind = graph.wind_graph
@@ -50,9 +53,17 @@ def getApiValue(request):
         
         s = {"labels":labels,"datasets":datasets}
         data.append(s)
-        
-        data = {"data":data}
-        return Response(data,status=status.HTTP_200_OK)
+
+        if len(myresult) > 0:
+            for x in myresult:
+                gd = {"date":x[8],"wind":x[1],"solar":x[2],"interstatewind":[3],"intrastatewind":[4],"interstatesolar":[5],"intrastatesolar":[6]}
+                graphD.append(gd)
+                if len(graphD) == len(myresult):
+                    data = {"data":data,"datagraph":graphD}
+                    return Response(data,status=status.HTTP_200_OK)
+        else:
+           data = {"data":data,"datagraph":graphD}
+           return Response(data,status=status.HTTP_200_OK)
    
 
                     
